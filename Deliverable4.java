2ed54863ef618e5e56aebfd99baa62a80f9021f1
Christian <cef39@pitt.edu>
2017-11-06 22:14:49 -0500

    commented out timing

--- Deliverable4.java
+++ Deliverable4.java
@@ -4,11 +4,11 @@ import java.util.concurrent.TimeUnit;
 
 public class Deliverable4{
 	public static void main(String args []){
-		long readFileTime = 0;
-		long hashMapTime = 0;
-		long checkElementsTime = 0;
-		long generateElementAbbrTime = 0;
-		long generateElementNameTime = 0;
+		//long readFileTime = 0;
+		//long hashMapTime = 0;
+		//long checkElementsTime = 0;
+		//long generateElementAbbrTime = 0;
+		//long generateElementNameTime = 0;
 
 		if (args.length != 1){
 			System.out.println("Error: Enter only one argument, the file to read.");
@@ -21,20 +21,20 @@ public class Deliverable4{
 		
 
 
-		long startTime = System.nanoTime();
+		//long startTime = System.nanoTime();
 		ArrayList<String> elements = Helpers.readFile(elementsFile);
 		ArrayList<String> input = Helpers.readFile(inputFile);
-		long endTime = System.nanoTime();
-		readFileTime = (endTime - startTime);
+		//long endTime = System.nanoTime();
+		//readFileTime = (endTime - startTime);
 
 		// if either are null, then there was a FileNotFoundException or IOException, so exit
 		if (elements == null || input == null)
 			System.exit(1);
 
-		startTime = System.nanoTime();
+		//startTime = System.nanoTime();
 		HashMap<String, String> elementsDict = Helpers.createHashMap(elements);
-		endTime = System.nanoTime();
-		hashMapTime = (endTime - startTime);
+		//endTime = System.nanoTime();
+		//hashMapTime = (endTime - startTime);
 
 		for (int i = 0; i < input.size(); i++){
 			String line = input.get(i);
@@ -42,20 +42,20 @@ public class Deliverable4{
 			if (line.equals(""))
 				continue;
 
-			startTime = System.nanoTime();
+			//startTime = System.nanoTime();
 			ArrayList<String> result = Helpers.checkElements(line, elementsDict);
-			endTime = System.nanoTime();
-			checkElementsTime += (endTime - startTime);
+			//endTime = System.nanoTime();
+			//checkElementsTime += (endTime - startTime);
 
-			startTime = System.nanoTime();
+			//startTime = System.nanoTime();
 			String elementAbbr = Helpers.generateElementAbbr(result);
-			endTime = System.nanoTime();
-			generateElementAbbrTime += (endTime - startTime);
+			//endTime = System.nanoTime();
+			//generateElementAbbrTime += (endTime - startTime);
 
-			startTime = System.nanoTime();
+			//startTime = System.nanoTime();
 			String elementNames = Helpers.generateElementName(result, elementsDict);
-			endTime = System.nanoTime();
-			generateElementNameTime += (endTime - startTime);
+			//endTime = System.nanoTime();
+			//generateElementNameTime += (endTime - startTime);
 
 			if (elementAbbr != null)
 				System.out.println(elementAbbr);
@@ -68,11 +68,11 @@ public class Deliverable4{
 			System.out.println();
 		}
 
-		System.out.println("readFile(): " + readFileTime);
-		System.out.println("createHashMap(): " + hashMapTime);
-		System.out.println("checkElements(): " + checkElementsTime);
-		System.out.println("generateElementAbbr(): " + generateElementAbbrTime);
-		System.out.println("generateElementNameTime(): " + generateElementNameTime);
+		/*System.out.println("readFile(): " + readFileTime + " ns");
+		System.out.println("createHashMap(): " + hashMapTime + " ns");
+		System.out.println("checkElements(): " + checkElementsTime + " ns");
+		System.out.println("generateElementAbbr(): " + generateElementAbbrTime + " ns");
+		System.out.println("generateElementNameTime(): " + generateElementNameTime + " ns");*/
 
 	}
 
